983f5df50143d5447e795521918d9f2ae85d02e1
hamata <ahmed.adham.1543@gmail.com>
2026-03-08 23:28:48 +0200

    init commit

---
+++ Domain/Common/Result.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Domain.Common
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; set; }
+        public T Value { get; set; }
+        public string? ErrorMessage { get; set; }
+
+        private Result(T value)
+        {
+            IsSuccess = true;
+            Value = value;
+            ErrorMessage = null;
+        }
+
+        private Result(string errorMessage)
+        {
+            IsSuccess = false;
+            Value = default(T);
+            ErrorMessage = errorMessage;
+        }
+
+        public static Result<T> Success(T value)
+        {
+            return new Result<T>(value);
+        }
+
+        public static Result<T> Failure(string errorMessage)
+        {
+            return new Result<T>(errorMessage);
+        }
+    }
+
+
+    public class Result
+    {
+        public bool IsSuccess { get; set; }
+        public string? ErrorMessage { get; set; }
+
+        private Result(bool isSuccess, string? error)
+        {
+            IsSuccess = isSuccess;
+            ErrorMessage = error;
+        }
+
+        public static Result Success()
+        {
+            return new Result(true, null);
+        }
+
+        public static Result Failure(string errorMessage)
+        {
+            return new Result(false, errorMessage);
+        }
+    }
+}
