3a895f79b9decd08c090ef305b10ddbd120e003d
hamata <ahmed.adham.1543@gmail.com>
2026-03-09 17:22:20 +0200

    Add folders

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+    }
+}
